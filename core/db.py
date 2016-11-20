a58aaefacc41d697e480a3c7d4671a1d3460e7f9
timorem <timorem@hotmail.fr>
2016-11-20 10:50:17 +0100

    ajout de commentaires

--- core/db.py
+++ core/db.py
@@ -2,12 +2,14 @@ import sqlalchemy as sql
 import docs.conf as cfg
 from sqlalchemy.ext.declarative import declarative_base
 
+# Créer la classe Base qui permet de créer le schema de la base de donnée
+# Elle est la classe fille de toute classe dont les objets sont stockés
 Base = declarative_base()
 
 _connection = None
 _engine = None
 
-
+# Retourne l'objet engine qui permet d'accéder au moteur qui fait le lien entre le code et la base de donnée
 def get_engine():
     global _engine
     if not _engine:
@@ -16,7 +18,7 @@ def get_engine():
                                                         cfg.db["database"]))
     return _engine
 
-
+# Retourne l'objet connection qui permet d'effecture des requetes à la base de données
 def get_connection():
     global _connection
     if not _connection:
--- core/entities/typology.py
+++ core/entities/typology.py
@@ -6,11 +6,17 @@ from sqlalchemy import Table, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from core.db import Base
 
+# Représente la résidence
+class Residence:
+    pass
+
+# Représente un batiment de la résidence
 class Building(Base):
     __tablename__ = 'buildings'
     id = Column(Integer, primary_key=True)
     bundles = relationship("Bundle", back_populates="building")
 
+# Représente un groupement de chambres (un étage ou une "grappe" de chambres)
 class Bundle(Base):
     __tablename__ = 'bundles'
     id = Column(Integer, primary_key=True)
@@ -18,6 +24,7 @@ class Bundle(Base):
     building_id = Column(Integer, ForeignKey('buildings.id'))
     building = relationship("Building", back_populates="bundles")
 
+# Représente une chambre
 class Room(Base):
     __tablename__ = 'rooms'
     id = Column(Integer, primary_key=True)
@@ -25,11 +32,9 @@ class Room(Base):
     bundle_id = Column(Integer, ForeignKey('bundles.id'))
     bundle = relationship("Bundle", back_populates="rooms")
 
+# Représente un résident
 class Resident(Base):
     __tablename__ = 'residents'
     id = Column(Integer, primary_key=True)
     room_id = Column(Integer, ForeignKey('rooms.id'))
     room = relationship("Room", back_populates="residents")
-
-class Residence:
-    pass
