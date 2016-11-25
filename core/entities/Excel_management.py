9a556130d0c1a231c3f4f8a5ff918866c54363e3
Merlin <merlin.laffitte@student.ecp.fr>
2016-11-25 09:27:08 +0100

    Excel_management commit

---
+++ core/entities/Excel_management.py
@@ -0,0 +1,53 @@
+import xlrd
+from xlwt import Workbook
+
+def lire_excel(nom_excel, numero_feuille, colonne, ligne):
+    wb = xlrd.open_workbook(nom_excel)
+    feuilles = wb.sheet_names()
+    #jusqu'ici tout est clair cependant noter que le numéro de feuille, de colonne et de ligne ira de 1 à n et non de 0 à n
+    feuille_i = wb.sheet_by_name(feuilles[numero_feuille -1])
+    colonne_i=feuille_i.col_values(colonne -1)
+    return colonne_i[ligne-1]
+
+
+def lire_nom_feuille(nom_excel):
+    wb = xlrd.open_workbook(nom_excel)
+    return wb.sheet_names()
+
+def lire_excel_col(nom_excel, numero_feuille, colonne):
+    wb = xlrd.open_workbook(nom_excel)
+    feuilles = wb.sheet_names()
+    #jusqu'ici tout est clair cependant noter que le numéro de feuille, de colonne et de ligne ira de 1 à n et non de 0 à n
+    feuille_i = wb.sheet_by_name(feuilles[numero_feuille -1])
+    colonne_i=feuille_i.col_values(colonne -1)
+    return colonne_i
+
+class excel_gestion():
+
+    def __init__(self, nom_feuille1) :
+        self.book = Workbook()
+        self.feuilles = []
+        self.feuilles.append(self.book.add_sheet(nom_feuille1))
+
+    def ecrire_excel(self, numero_feuille, ligne, colonne, element):
+        lignei = self.feuilles[numero_feuille-1].row(ligne-1)
+        lignei.write(colonne-1,element)
+
+    def ajout_feuille(self, nom_feuille):
+        self.feuilles.append(self.book.add_sheet(nom_feuille))
+
+    def creation (self, nom):
+        self.book.save(nom)
+
+print("test")
+
+#print(lire_excel("Test2.xlsx", 1, 1, 1))
+
+#print(lire_excel("Test2.xlsx", 2, 2, 2))
+
+#print (lire_nom_feuille("Test2.xlsx"))
+Excel = excel_gestion ("feuille1")
+Excel.ajout_feuille("feuille2")
+Excel.ecrire_excel(1,1,1, "TOTO")
+Excel.ecrire_excel(2,2,2,"TITI")
+Excel.creation("Test3")
