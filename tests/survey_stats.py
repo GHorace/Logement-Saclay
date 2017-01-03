2b6cf839533a569271788b1c0337e8c8298e8295
timorem <timorem@hotmail.fr>
2017-01-03 18:18:26 +0100

    add residents file add survey csv

--- tests/survey_stats.py
+++ tests/survey_stats.py
@@ -2,20 +2,39 @@ import pandas
 
 import matplotlib.pyplot as plt
 
-from sklearn import metrics
-from sklearn.cluster import KMeans
+from sklearn.datasets import make_classification
+from sklearn.ensemble import ExtraTreesClassifier
+import numpy as np
 
 df = pandas.read_csv('../docs/survey.csv')
 
 def isnan(x):
     return x != x
 
+lastID = 0
+def coding(x, code):
+    global lastID
+    if isinstance(x,str):
+        if not x in code:
+            lastID += 1
+            code[x] = lastID
+
+        return code[x]
+
+    if isnan(x):
+        return 0
+
+    return x
+
+
+# replace values
+code = {}
+for column in df:
+    lastID = 0
+    df[column].map(lambda x: coding(x, code))
+
 results = list()
 for row in df.values:
-    noteCommunity = row[4]
-    noteParty = row[5]
-    noteWork = row[6]
-
     i = 21
     while isnan(row[i]):
         i += 1
@@ -31,8 +50,34 @@ for row in df.values:
             if not isnan(row[i]):
                 floor = str(i - 27) + str(row[i])
                 desiredFloors.append(floor)
-                results.append((noteCommunity, noteParty, noteWork, floor))
+                results.append(row[1:20].tolist() + [floor])
     else:
-        results.append((noteCommunity, noteParty, noteWork, currentFloor))
+        results.append(row[1:20].tolist() + [currentFloor])
 
-print(results)
+vectCoding = np.vectorize(coding)
+results = vectCoding(np.array(results), code)
+forest = ExtraTreesClassifier(n_estimators = 100)
+X = results[0::,1:-1]
+y = results[0::,-1]
+
+forest = forest.fit(X,y)
+
+importances = forest.feature_importances_
+std = np.std([tree.feature_importances_ for tree in forest.estimators_],
+             axis=0)
+indices = np.argsort(importances)[::-1]
+
+# Print the feature ranking
+print("Feature ranking:")
+
+for f in range(X.shape[1]):
+    print("%d. feature '%s' (%f)" % (f + 1, df.columns[indices[f]], importances[indices[f]]))
+
+# Plot the feature importances of the forest
+plt.figure()
+plt.title("Feature importances")
+plt.bar(range(X.shape[1]), importances[indices],
+       color="r", yerr=std[indices], align="center")
+plt.xticks(range(X.shape[1]), indices)
+plt.xlim([-1, X.shape[1]])
+plt.show()
